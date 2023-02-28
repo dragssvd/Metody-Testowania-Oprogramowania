682b0ccd4dd2bb8afd36795c888dcf161353062f
Baban <97313850+dragssvd@users.noreply.github.com>
2023-02-28 18:34:25 +0100

    Dodanie zamiany wielkości znaków do printf

--- lab1/main.py
+++ lab1/main.py
@@ -8,10 +8,16 @@ def my_printf(format_string,param):
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
+                if param.isupper():
+                    print(param.lower(),end="")
+                else:
+                    print(param.upper(),end="")
                 shouldDo=False
             else:
-                print(format_string[idx],end="")
+                if format_string[idx].isupper():
+                    print(format_string[idx].lower(),end="")
+                else:
+                    print(format_string[idx].upper(),end="")
         else:
             shouldDo=True
     print("")
