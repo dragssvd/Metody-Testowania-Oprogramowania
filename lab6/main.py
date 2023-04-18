dd14604ee942bd555ff44f6f1987f7a741bc794b
Baban <dragssvd@gmail.com>
2023-04-18 18:13:39 +0200

    add param len limiter

--- lab6/main.py
+++ lab6/main.py
@@ -15,13 +15,34 @@ def my_printf(format_string,param):
     else:
         if negative:
             param = param[1:]
+
+        replace = '#.'
+        param_limit = 0
+        for idx in range(0,len(format_string)):
+            if format_string[idx] == '#' and format_string[idx+1] == '.':
+                i=0
+                while format_string[idx+2+i].isnumeric():
+                    replace = replace + str(format_string[idx+2+i])
+                    i+=1
+                param_limit = int(format_string[idx+2:idx+2+i])
+        
+        replace = replace + 'g'
+
         nparam=''
+        if param_limit > len(param):
+            for x in range(0, param_limit-len(param)):
+                nparam = nparam + '0'
+        
         for c in param:
-            nparam += str(convert(int(c)))
+            nparam = nparam + str(convert(int(c)))
+
+        if len(param) > param_limit:
+            nparam = nparam[0:param_limit]
 
         if negative:
             nparam = '-' + nparam
-        print(format_string.replace('#.Xg', str(nparam)))
+            
+        print(format_string.replace(replace, str(nparam)))
 
 data=sys.stdin.readlines()
 
