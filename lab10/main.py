934a40c4961927f6c72e0d01e06d1971253cdaa3
Baban <dragssvd@gmail.com>
2023-05-30 17:34:01 +0200

    implement new param func

--- lab10/main.py
+++ lab10/main.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
 
 import sys
-
+    
+def new_param(number):
+    return int((number*2)/len(str(number)))
+    
 def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
-        else:
-            shouldDo=True
-    print("")
+    try:
+        n = int(param)
+        negative = True if n < 0 else False 
+    except:
+        print(format_string)
+    else:
+        if negative:
+            param = param[1:]
+
+        replace = '#a'
+
+        param = new_param(param)%2
+        if param != 0:
+            param = str(hex(param)).replace('0x', '')
+            
+        if negative:
+            nparam = '-' + nparam
+            
+        print(format_string.replace(replace, str(nparam)))
 
 data=sys.stdin.readlines()
 
